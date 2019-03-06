f11fa8db166f534443a8a28cafeb4008d0420e55
sdim-ua <ingersol@yandex.ua>
2019-03-06 15:17:31 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //напишите тут ваш код
+            ArrayList <String> list1 = new  ArrayList <String>(); 
+        return list1;
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+            LinkedList <String> list2 = new  LinkedList <String>();
+        return list2;    
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
